5be781cf8ae4794ac4f7bd1bbda77fa59ae82ab3
Aseel Alasaad <aseelalasaad@gmail.com>
2021-12-13 18:37:20 +0200

    lab23

--- src/context/Settings.js
+++ src/context/Settings.js
@@ -6,10 +6,13 @@ export const SettingContext = React.createContext();
 function Settings(props){
 const [pagination, setPagination]=useState(2);
 const [hide, setHide]=useState(false);
+const [listComplete, setlistComplete]=useState([]);
+
 // const toggleComplete=()=>setHide(hide===true?false:true)
 const state={
     pagination:2,
-    hide:false
+    hide:false,
+    listComplete:[]
 }
 
 return(
